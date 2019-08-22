2fc89197f447738a685655a592dc0db477557961
Bryant Plano <bryant+dev@ionic.io>
2019-08-22 15:26:38 -0500

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: Http) {
 
+  }
+
+  makeApiCall() {
+    const url = 'http://numbersapi.com/42';
+    this.http.get(url).subscribe((response) => {
+      console.log(response);
+    });
   }
 
 }
